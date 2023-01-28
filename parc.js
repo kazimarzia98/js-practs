42be2f546714195e115c35ede986a3f207ddcde7
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-01-28 11:34:48 +0600

    js file

---
+++ parc.js
@@ -0,0 +1,273 @@
+
+
+
+
+// // practs1
+
+// var alia = 95;
+// var dalia = 66;
+// var salia = 80; 
+// var malia = 59; 
+// var lilia = 47; 
+// var jalia = 77; 
+
+// // alia 
+// if(alia < 50){
+//     console.log(' Alia gets Grade F');
+// }
+
+//     else  if(alia >= 50 && alia < 60){
+//         console.log(' Alia gets Grade D');
+//     }
+//     else  if(alia >= 60 && alia < 70){
+//         console.log( 'Alia gets Grade C');
+//     }
+//     else  if(alia >= 70 && alia < 80){
+//         console.log('Alia gets Grade B');
+//     }
+//     else  if(alia >= 80 && alia < 90){
+//         console.log('Alia gets Grade A');
+//     }
+//     else  if(alia >= 90){
+//         console.log('Alia gets Grade A+');
+//     }
+
+// // dalia 
+
+//     if(dalia < 50){
+//         console.log(' Dalia gets Grade F');
+//     }
+    
+//         else  if(dalia >= 50 && dalia < 60){
+//             console.log(' DAlia gets Grade D');
+//         }
+//         else  if(dalia >= 60 && dalia < 70){
+//             console.log( 'DAlia gets Grade C');
+//         }
+//         else  if(dalia >= 70 && dalia < 80){
+//             console.log('DAlia gets Grade B');
+//         }
+//         else  if(dalia >= 80 && dalia < 90){
+//             console.log('DAlia gets Grade A');
+//         }
+//         else  if(dalia >= 90){
+//             console.log('DAlia gets Grade A+');
+//         }
+
+
+
+//         // salia 
+//         if(salia < 50){
+//             console.log(' Dalia gets Grade F');
+//         }
+        
+//             else  if(salia >= 50 && salia < 60){
+//                 console.log(' SAlia gets Grade D');
+//             }
+//             else  if(salia >= 60 && salia < 70){
+//                 console.log( 'SAlia gets Grade C');
+//             }
+//             else  if(salia >= 70 && salia < 80){
+//                 console.log('SAlia gets Grade B');
+//             }
+//             else  if(salia >= 80 && salia < 90){
+//                 console.log('SAlia gets Grade A');
+//             }
+//             else  if(salia >= 90){
+//                 console.log('SAlia gets Grade A+');
+//             }
+
+// // malia 
+
+//             if(malia < 50){
+//                 console.log(' malia gets Grade F');
+//             }
+            
+//                 else  if(malia >= 50 && malia < 60){
+//                     console.log(' mAlia gets Grade D');
+//                 }
+//                 else  if(malia >= 60 && malia < 70){
+//                     console.log( 'mAlia gets Grade C');
+//                 }
+//                 else  if(malia >= 70 && malia < 80){
+//                     console.log('mAlia gets Grade B');
+//                 }
+//                 else  if(malia >= 80 && malia < 90){
+//                     console.log('mAlia gets Grade A');
+//                 }
+//                 else  if(malia >= 90){
+//                     console.log('mAlia gets Grade A+');
+//                 }
+
+// // lilia 
+
+//                 if(lilia < 50){
+//                     console.log(' lilia gets Grade F');
+//                 }
+                
+//                     else  if(lilia >= 50 && lilia < 60){
+//                         console.log(' lilia gets Grade D');
+//                     }
+//                     else  if(lilia >= 60 && lilia < 70){
+//                         console.log( 'lilia gets Grade C');
+//                     }
+//                     else  if(lilia >= 70 && dlilia < 80){
+//                         console.log('lilia gets Grade B');
+//                     }
+//                     else  if(lilia >= 80 && lilia < 90){
+//                         console.log('lilia gets Grade A');
+//                     }
+//                     else  if(lilia >= 90){
+//                         console.log('lilia gets Grade A+');
+//                     }
+    
+//                     // jaliya 
+
+//                     if(jalia < 50){
+//                         console.log(' jalia gets Grade F');
+//                     }
+                    
+//                         else  if(jalia >= 50 && jalia < 60){
+//                             console.log(' jAlia gets Grade D');
+//                         }
+//                         else  if(jalia >= 60 && jalia < 70){
+//                             console.log( 'jAlia gets Grade C');
+//                         }
+//                         else  if(jalia >= 70 && jalia < 80){
+//                             console.log('jAlia gets Grade B');
+//                         }
+//                         else  if(jalia >= 80 && jalia < 90){
+//                             console.log('jAlia gets Grade A');
+//                         }
+//                         else  if(jalia >= 90){
+//                             console.log('jAlia gets Grade A+');
+//                         }
+
+//                         else{
+//                             console.log('user not found');
+//                         }
+        
+
+
+
+
+// practs 2 
+
+// var signal ='green';
+
+
+// if(signal === 'green'){
+//     console.log("cross the road");
+// }
+// else if(signal === 'yellow'){
+//     console.log("stop and wait for next signal");
+// }
+// else if(signal === 'red'){
+//     console.log('stop,it could be danger');
+// }
+// else{
+//     console.log('invalid signal');
+// }
+
+
+// var signal ='yellow';
+
+
+// if(signal === 'green'){
+//     console.log("cross the road");
+// }
+// else if(signal === 'yellow'){
+//     console.log("stop and wait for next signal");
+// }
+// else if(signal === 'red'){
+//     console.log('stop,it could be danger');
+// }
+// else{
+//     console.log('invalid signal');
+// }
+
+
+// var signal ='red';
+
+
+// if(signal === 'green'){
+//     console.log("cross the road");
+// }
+// else if(signal === 'yellow'){
+//     console.log("stop and wait for next signal");
+// }
+// else if(signal === 'red'){
+//     console.log('stop,it could be danger');
+// }
+// else{
+//     console.log('invalid signal');
+// }
+
+
+// practs from module vdo 1 
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// var position = fruits.indexOf('banana');
+
+// console.log(position);
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// fruits[1] = 'mango';
+
+// console.log(fruits);
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// fruits.push('watermelon');
+// console.log(fruits);
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// fruits.pop('orange');
+// console.log(fruits);
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// fruits.shift('watermelon');
+// console.log(fruits);
+
+// var fruits = ['apple' , 'banana' , 'orange'];
+
+// fruits.unshift('watermelon');
+// console.log(fruits);
+
+
+
+// parcts from module 3 
+
+
+// var num1 = 90;
+// var num2 = 79 ;
+// var num3 = 85 ;
+
+// if( num1 > num2 && num1 > num3){
+// console.log('num1 is largest number');
+// }
+// else if( num2 > num1 && num2 > num3){
+//     console.log('num2 is largest number');
+// }
+// else if(num3 > num1 && num3 > num2){
+//     console.log('num3 is largest number');
+// }
+// else{
+//     console.log('invalid');
+// }
+
+
+// var a = 9;
+// var b = 8;
+// var c = 9;
+
+// if( a === b || a === c || b === c){
+//     console.log('isosceles');
+// }
+// else{
+//     console.log('none');
+// }
