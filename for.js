3833a260feb9d1906bed07a6a7aba93edf761fb1
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-01-28 13:58:59 +0600

    more js

---
+++ for.js
@@ -0,0 +1,41 @@
+// while loop 
+
+// var roastGiven = 0;
+// while(roastGiven < 7 ){
+//     console.log('roast den pls ..');
+//     roastGiven++;
+// }
+
+// for loop 
+
+// for(var roastGiven = 0; roastGiven < 7 ; roastGiven++){
+//     console.log('roast den pls..');
+// }
+
+// 
+
+// i = i + 2
+// i += 2
+
+// for(var i =  0 ; i <= 20; i +=2){
+//     console.log(i);
+// }
+
+
+// display every element of an array
+
+var numbers = [40, 34, 43, 74,24,52, 42,63, 63,87];
+
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+
+
+var items =['bottle' ,'net' , 'box' , 'sunglass' , 'glass'];
+
+for( i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
---
+++ loop.js
@@ -0,0 +1,23 @@
+// display 1 to 10
+
+// var number = 1;
+// while(number <= 10){
+//      console.log(number);
+//      number++;
+// }
+
+// even 
+// var number = 0;
+// while(number <= 20){
+//      console.log(number);
+//     //  number++;
+//     number = number + 2;
+// }
+
+// odd 
+var number = 1;
+while(number <= 20){
+     console.log(number);
+    //  number++;
+    number = number + 2;
+}
