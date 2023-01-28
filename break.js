099d024972d45f7ec1e60afc9c347653c0e01c3e
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-01-28 14:14:31 +0600

    more js

---
+++ break.js
@@ -0,0 +1,48 @@
+// break 
+// break means that stop at that moment 
+
+// for(i = 1; i < 10; i++){
+//     console.log(i);
+//     if(i > 4){
+//         break;
+//     }
+// }
+
+
+// var items =['bottle' ,'net' , 'box' , 'sunglass' , 'glass'];
+
+// for( i = 0; i < items.length; i++){
+//     var item = items[i];
+    
+//     if(item=='box'){
+//         break;
+//     }
+//     console.log(item);
+// }
+
+
+
+// var numbers = [40, 34, 43, 74,24,52, 42,63, 63,87];
+
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     if(number > 60){
+//         break;
+//     }
+//     console.log(number);
+// }
+
+
+
+// continue 
+// continue means skip that item 
+
+var numbers = [40, 34, 43, 74,24,52, 42,63, 63,87];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    if(number > 60){
+        continue;
+    }
+    console.log(number);
+}
