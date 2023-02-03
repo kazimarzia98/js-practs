1a48b772bec7d6984b687f9ffe057b742fd7adfe
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-02-03 19:46:52 +0600

    more js

---
+++ swap.js
@@ -0,0 +1,15 @@
+// if want to swap number then use temp
+
+
+let first = 5;
+let second = 7;
+
+console.log(first, second);
+
+//  use temp to swap number,means first number will be secnd nmbr ,secnd nmbr will be first nmbr 
+// temp means temporary
+
+const temp = first;
+first = second;
+second = temp;
+console.log(first, second);
