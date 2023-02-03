57da916a2d1ec398d72d5859db1b32f3b4d48fc0
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-02-03 16:19:59 +0600

    js more

---
+++ math.js
@@ -0,0 +1,25 @@
+const result = Math.pow(3,8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+// const gap = num1-num2;
+const gap = Math.abs(num1-num2);
+
+if(gap < 5){
+    console.log('u guys can be friends');
+}
+else{
+    console.log('u guys stay apart');
+}
+
+
+
+
+// .....
+
+const number = 2.467;
+const fullNumber = Math.round(number);
+console.log(fullNumber);
+
